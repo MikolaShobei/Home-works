0baad0a88fa6c78a8d7a95e245929196d55a01ed
MikolaShobei <mikola.shobey.wr>
2020-10-28 17:35:30 +0200

    Add folder dz-5

---
+++ Dz-5_JS_1/JS-1.js
@@ -0,0 +1,405 @@
+
+{//           Завдання 1
+
+// let hello = "hello";
+// let owu = "owu";
+// let com = "com";
+// let ua = "ua";
+// let nomber1 = 1;
+// let nomber10 = 10;
+// let nomber3 = -999;
+// let nomber4 = 123;
+// let pi = 3.14;
+// let chislo = 3.7;
+// let nomber16 = 16;
+// let isMouse = true;
+// let isPivo = false;
+
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(nomber1);
+// alert(nomber10);
+// alert(nomber3);
+// alert(nomber4);
+// alert(pi);
+// alert(chislo);
+// alert(nomber16);
+// alert(isMouse);
+// alert(isPivo);
+// document.write(hello);
+// document.write("<br>");
+// document.write(owu);
+// document.write("<br>");
+// document.write(com);
+// document.write("<br>");
+// document.write(ua);
+// document.write("<br>");
+// document.write(nomber1);
+// document.write("<br>");
+// document.write(nomber10);
+// document.write("<br>");
+// document.write(nomber3);
+// document.write("<br>");
+// document.write(nomber4);
+// document.write("<br>");
+// document.write(pi);
+// document.write("<br>");
+// document.write(chislo);
+// document.write("<br>");
+// document.write(nomber16);
+// document.write("<br>");
+// document.write(isMouse);
+// document.write("<br>");
+// document.write(isPivo);
+// document.write("<br>");
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(nomber1);
+// console.log(nomber10);
+// console.log(nomber3);
+// console.log(nomber4);
+// console.log(pi);
+// console.log(chislo);
+// console.log(nomber16);
+// console.log(isMouse);
+// console.log(isPivo);
+}
+
+{    //   Завдання 2
+
+    // let hello = "hello";
+    // let owu = "owu";
+    // let com = "com";
+    // let ua = "ua";
+    // let nomber1 = 1;
+    // let nomber10 = 10;
+    // let nomber3 = -999;
+    // let nomber4 = 123;
+    // let pi = 3.14;
+    // let chislo = 3.7;
+    // let nomber16 = 16;
+    // let isMouse = true;
+    // let isPivo = false;
+
+
+    // hello = "Здоров";
+    // owu = "Valar morhulis";
+    // com = "com.ua";
+    // ua = "Україна";
+    // nomber1 = "Одинадцять";
+    // nomber10 = '10';
+    // nomber3 = "-999";
+    // nomber4 = "Сто двадцять три";
+    // pi = "3.14159265359";
+    // chislo = 2.718;
+    // nomber16 = 160000;
+    // isMouse = false;
+    // isPivo = true;
+    
+    // alert(hello);
+    // alert(owu);
+    // alert(com);
+    // alert(ua);
+    // alert(nomber1);
+    // alert(nomber10);
+    // alert(nomber3);
+    // alert(nomber4);
+    // alert(pi);
+    // alert(chislo);
+    // alert(nomber16);
+    // alert(isMouse);
+    // alert(isPivo);
+    // document.write(hello);
+    // document.write("<br>");
+    // document.write(owu);
+    // document.write("<br>");
+    // document.write(com);
+    // document.write("<br>");
+    // document.write(ua);
+    // document.write("<br>");
+    // document.write(nomber1);
+    // document.write("<br>");
+    // document.write(nomber10);
+    // document.write("<br>");
+    // document.write(nomber3);
+    // document.write("<br>");
+    // document.write(nomber4);
+    // document.write("<br>");
+    // document.write(pi);
+    // document.write("<br>");
+    // document.write(chislo);
+    // document.write("<br>");
+    // document.write(nomber16);
+    // document.write("<br>");
+    // document.write(isMouse);
+    // document.write("<br>");
+    // document.write(isPivo);
+    // document.write("<br>");
+    // console.log(hello);
+    // console.log(owu);
+    // console.log(com);
+    // console.log(ua);
+    // console.log(nomber1);
+    // console.log(nomber10);
+    // console.log(nomber3);
+    // console.log(nomber4);
+    // console.log(pi);
+    // console.log(chislo);
+    // console.log(nomber16);
+    // console.log(isMouse);
+    // console.log(isPivo);
+
+}
+
+
+{       //Завдання 3
+    // let pi = 3.14159265359;
+    // let exp = 2.718;
+    // let Plank = 1.61624 * 10 * Math.E-35
+    // alert(pi);
+    // alert(exp);
+    // alert(Plank);
+    // document.write(pi);
+    // document.write("<br>");
+    // document.write(exp);
+    // document.write("<br>");
+    // document.write(Plank);
+    // console.log(pi);
+    // console.log(exp);
+    // console.log(Plank);
+
+    // pi = "3.14159265359";
+    // exp =" 2.718";
+    // Plank = "1.61624 * 10 * Math.E-35"
+    // alert(pi);
+    // alert(exp);
+    // alert(Plank);
+    // document.write(pi);
+    // document.write("<br>");
+    // document.write(exp);
+    // document.write("<br>");
+    // document.write(Plank);
+    // console.log(pi);
+    // console.log(exp);
+    // console.log(Plank);
+
+} 
+
+{      // Завдання 4
+    // let first_name = prompt("Введіть Прізвище");
+    // let name = prompt("Введіть ім'я");
+    // let father_name = prompt("Введіть по-батькові");
+    // alert(first_name +" " + name + " " + father_name);
+    // document.write(first_name +" " + name + " " + father_name);
+    // console.log(first_name +" " + name + " " + father_name);
+}
+
+{       // Завдання 5
+    // let first_name = prompt("Введіть Прізвище");
+    // let name = prompt("Введіть ім'я");
+    // let father_name = prompt("Введіть по-батькові");
+    // let person = first_name +" " + name + " " + father_name;
+    // alert(person);
+    // document.write(person);
+    // console.log(person);
+}
+
+
+{           //   Завдання 6
+            //Як я зрозумів завдання так і виконав, якщо його треба було інакше то скажіть будь ласка і я перероблю))))
+    // let father = prompt("Введіть ПІБ тата");
+    // let mather = prompt("Введіть ПІБ мами");
+    // let sister = prompt("Введіть ПІБ сестри");
+    // let bro = prompt("Введіть ПІБ брата");
+    // let me = prompt("Введіть своє ПІБ");
+    // alert(father);
+    // document.write(father);
+    // console.log(father);
+    // document.write("<br>");
+    // alert(mather);
+    // document.write(mather);
+    // console.log(mather);
+    // document.write("<br>");
+    // alert(sister);
+    // document.write(sister);
+    // console.log(sister);
+    // document.write("<br>");
+    // alert(bro);
+    // document.write(bro);
+    // console.log(bro);
+    // document.write("<br>");
+    // alert(me);
+    // document.write(me);
+    // console.log(me);
+    
+
+
+}
+
+{   //Завдання 7
+    // let chislo_1 = prompt("Введіть число 1");
+    // let chislo_2 = prompt("Введіть число 2");
+    // let chislo_3 = prompt("Введіть число 3");
+    // console.log(+chislo_1);
+    // console.log(+chislo_2);
+    // console.log(+chislo_3);
+
+}
+{   //Завдання 8
+
+    // let chislo_1 = prompt("Введіть число 1");
+    // let chislo_2 = prompt("Введіть число 2");
+    // let chislo_3 = prompt("Введіть число 3");
+    // let chislo_4 = prompt("Введіть число 4");
+    // let sum = parseInt(chislo_1) + parseInt(chislo_2) + parseInt(chislo_3)+ parseInt(chislo_4);
+    // console.log(sum);
+   
+
+}
+
+{   //Завдання 9
+
+    // let chislo_1 = prompt("Введіть число float 1");
+    // let chislo_2 = prompt("Введіть число float 2");
+    // let chislo_3 = prompt("Введіть число float 3");
+    // let sum = parseFloat(chislo_1) + parseFloat(chislo_2) + parseFloat(chislo_3);
+    // console.log(sum);
+   
+
+}
+{   //Завдання 10
+
+    // let chislo_1 = +prompt("Введіть число float 1");
+    // let chislo_2 = +prompt("Введіть число float 2");
+    // let chislo_3 = +prompt("Введіть число float 3");
+    // let sum = Math.round(chislo_1) + Math.round(chislo_2) + Math.round(chislo_3);
+    // console.log(sum);
+   
+
+}
+{   //Завдання 11
+
+    // let chislo_1 = +prompt("Введіть число Int 1");
+    // let chislo_2 = +prompt("Введіть число Int 2");
+    // let sum = Math.pow(chislo_1,chislo_2)
+    // console.log(sum);
+   
+
+}
+{   //Завдання 12
+
+//     let a = 100; 
+//     let b = '100'; 
+//     let c = true; 
+//     let d = undefined;
+//     console.log("a -- " + typeof(a));
+//     console.log("b -- " + typeof(b));
+//     console.log("c -- " + typeof(c));
+//     console.log("d -- " + typeof(d));
+
+}
+
+{   //Завдання 13
+
+    // console.log(5 < 6);
+    // console.log(5 == 6);
+    // console.log(5 > 6);
+    // console.log(5 >= 6);
+    // console.log(10 == 10);
+    // console.log(10 === 10);
+    // console.log(10 != 10);
+    // console.log(10 < 10);
+    // console.log(10 > 10);
+    // console.log(123 === "123");
+    // console.log(123 == "123");
+
+}
+
+
+
+
+
+                                ///ДОП
+
+{        //   Завдання 1
+    // let a =prompt("Введіть число 1")
+    // let b =prompt("Введіть число 2")
+    // let c =prompt("Введіть число 3")
+    // let first, second, third; 
+
+    // if ( a < b){
+    //     first = a;
+    //     second = b;
+    // }else{
+    //     first = b;
+    //     second = a;
+    // }
+    // if (c < first){
+    //    third = second;
+    //    second = first;
+    //    first = c;        
+    // }
+    // if (first < c && c < second) {
+    //     third = second;
+    //     second = c;
+        
+    // }
+    // if (c > first && c > second) {
+    //     third = c;
+    // }
+
+
+    // console.log(first + " " + second + " " + third) 
+
+}
+
+{            //   Завдання 2
+
+    // let comand = prompt(" Що на світлофорі? ")
+    // if (comand =="зелений"){
+    //     confirm("іди")
+    // }
+    // if (comand =="жовтий"){
+    //     confirm("зачекай")
+    // }
+    // if (comand =="червоний"){
+    //     confirm("стій")
+    // }
+    // if (comand =="зламаний"){
+    //     confirm("Роби що хочеш")
+    // }
+
+}
+{            //   Завдання 3
+
+    // let comand1 = prompt(" Що на світлофорі?   зелений, жовтий, червоний, зламаний")
+    // let comand2 = prompt(" Є авто?   так або ні")
+
+    // if (comand1 =="зелений" && comand2 == "ні" ){
+    //     confirm("іди")
+    // }if (comand1 =="зелений" && comand2 == "так" ){
+    //     confirm("подожди пока нарушители проедут")
+    // }
+    // if (comand1 =="жовтий" && comand2 == "так"){
+    //     confirm("зачекай")
+    // }
+    // if (comand1 =="жовтий" && comand2 == "ні"){
+    //     confirm("все рівно зачекай")
+    // }
+    // if (comand1 =="червоний" && comand2 == "так"){
+    //     confirm("стій")
+    // }
+    // if (comand1 =="червоний" && comand2 == "ні"){
+    //     confirm("стій все рівно")
+    // }
+    // if (comand1 =="зламаний"){
+    //     confirm("Роби що хочеш")
+    // }
+
+}
+
+
